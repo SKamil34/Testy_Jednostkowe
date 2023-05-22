e13238f2ad04ec535b0eaa3ed05ff450f6f58bc5
kamil <kamil@SKAMIL>
2023-05-22 22:15:32 +0200

    Dodaj pliki projektów.

---
+++ Pierwiastki/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class ObliczanieDelty
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj współczynniki równania kwadratowego:");
+        Console.Write("Podaj wartość a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj wartość b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj wartość c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        double delta = b * b - 4 * a * c;
+
+        var pierwiastek = ObliczDelte(delta, a, b);
+
+        Console.WriteLine($"Delta jest równa: {delta}");
+        Console.WriteLine(pierwiastek);
+        Console.ReadLine();
+    }
+
+    public static string ObliczDelte(double delta, double a, double b)
+    {
+        if (delta > 0)
+        {
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            return ($"Pierwiastki równania kwadratowego to: x1 = {x1}, x2 = {x2}");
+        }
+        else if (delta == 0)
+        {
+            double x = -b / (2 * a);
+            return ($"Równanie kwadratowe ma jeden pierwiastek: x = {x}");
+        }
+        return ("Równanie kwadratowe nie ma pierwiastków rzeczywistych.");
+        
+    }
+
+}
---
+++ Testy_JednostkoweFinal/UnitTest1.cs
@@ -0,0 +1,55 @@
+using Xunit;
+using Xunit.Sdk;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        private readonly ObliczanieDelty _program;
+        public UnitTest1()
+        {
+            _program = new ObliczanieDelty();
+        }
+        [Fact]
+        public void ObliczDelte_Poprawnie_Zwraca_2_pierwiastki()
+        {
+            // arrange
+            double delta = 64;
+            double a = 4;
+            double b = 8;
+            // act
+            var Test1 = ObliczanieDelty.ObliczDelte(delta, a, b);
+            // assert
+            Assert.NotNull(Test1);
+            Assert.Equal("Pierwiastki równania kwadratowego to: x1 = 0, x2 = -2", Test1);
+        }
+        [Fact]
+        public void ObliczDelte_Poprawnie_Zwraca_1_pierwiastek()
+        {
+            double delta = 0;
+            double a = 1;
+            double b = 4;
+            var Test2 = ObliczanieDelty.ObliczDelte(delta, a, b);
+            Assert.NotNull(Test2);
+            Assert.Equal("Równanie kwadratowe ma jeden pierwiastek: x = -2", Test2);
+        }
+        [Fact]
+        public void ObliczDelte_Brak_Pierwiastkow()
+        {
+            double delta = -139;
+            double a = -1;
+            double b = -1;
+            var Test3 = ObliczanieDelty.ObliczDelte(delta, a, b);
+            Assert.NotNull(Test3);
+            Assert.Equal("Równanie kwadratowe nie ma pierwiastków rzeczywistych.", Test3);
+        }
+        [Theory]
+        [InlineData(0,1,4)]
+        public void Zwraca_2(double delta, double a, double b) 
+        {
+            var Test4 = ObliczanieDelty.ObliczDelte(delta, a, b);
+            Assert.NotNull(Test4);
+            Assert.Equal("Równanie kwadratowe ma jeden pierwiastek: x = -2", Test4);
+        }
+    }
+}
